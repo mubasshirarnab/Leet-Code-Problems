958543c5dae2a07ef0d163aed813cd14d18d90b4
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2023-11-04 01:13:37 +0600

    Add files via upload
    Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
    
    Example 1:
    
    Input: haystack = "sadbutsad", needle = "sad"
    Output: 0
    Explanation: "sad" occurs at index 0 and 6.
    The first occurrence is at index 0, so we return 0.
    Example 2:
    
    Input: haystack = "leetcode", needle = "leeto"
    Output: -1
    Explanation: "leeto" did not occur in "leetcode", so we return -1.

--- Solution.java
+++ Solution.java
@@ -1,53 +1,35 @@
-package HardProblems;
-
-
-/*      Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
-                The overall run time complexity should be O(log (m+n)).
-
-                Input: nums1 = [1,2], nums2 = [3,4]
-                Output: 2.50000
-                Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.  */
-
 public class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i = 0, j = 0, k = 0;
-        int[] sortedMergedArray = new int[nums1.length + nums2.length];
-
-        while (i < nums1.length && j < nums2.length){
-            if (nums1[i] < nums2[j]){
-                sortedMergedArray[k] = nums1[i];
-                i++;
+    public void strStr(String haystack, String needle) {
+        int flag = 0, index = 0;
+        if(haystack.length() == 1){
+            System.out.println("0");
+        }
+        else {
+            for (int i = 0; i < haystack.length(); i++){
+                for (int j = i; j < haystack.length() + 1; j++){
+                    String s = haystack.substring(i, j);
+                    if (s.equals(needle)){
+                        flag = 1;
+                        index = i;
+                        break;
+                    }
+                    if (flag == 1){
+                        break;
+                    }
+                }
             }
-            else {
-                sortedMergedArray[k] = nums2[j];
-                j++;
+            if (flag == 1){
+                System.out.println(index);
+            }else {
+                System.out.println("-1");
             }
-            k++;
         }
-        while (i < nums1.length){
-            sortedMergedArray[k] = nums1[i];
-            i++;
-            k++;
         }
-        while (j < nums2.length){
-            sortedMergedArray[k] = nums2[j];
-            j++;
-            k++;
-        }
-
-        if (sortedMergedArray.length % 2 == 1){
-            return sortedMergedArray[sortedMergedArray.length / 2];
-        }
-        else{
-            return ((double) sortedMergedArray[sortedMergedArray.length / 2]
-                    + (double)sortedMergedArray[(sortedMergedArray.length / 2)-1]) / 2;
-        }
-    }
 }
 
-class Test{
+class Main100{
     public static void main(String[] args) {
-        Solution S = new Solution();
-        System.out.println(S.findMedianSortedArrays(new int[]{1,2}, new int[]{3,4}));
+        Solution s = new Solution();
+        s.strStr("sadbutsad", "sad");
     }
 }
