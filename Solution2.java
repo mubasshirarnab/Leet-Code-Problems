3cc2cf2f3cf0e076671f3a2b60942e4b8919d313
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2024-01-30 20:04:10 +0600

    Add files via upload
    Given an input string s, reverse the order of the words. A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
    Return a string of the words in reverse order concatenated by a single space.
    
    Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
    
    Example 1:
    Input: s = "the sky is blue"
    Output: "blue is sky the"
    
    Example 2:
    Input: s = "  hello world  "
    Output: "world hello"
    Explanation: Your reversed string should not contain leading or trailing spaces.
    
    Example 3:
    Input: s = "a good   example"
    Output: "example good a"
    Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
     
    
    Constraints:
    
    1 <= s.length <= 104
    s contains English letters (upper-case and lower-case), digits, and spaces ' '.
    There is at least one word in s.

--- Solution2.java
+++ Solution2.java
@@ -1,23 +1,40 @@
-package EasyProblems;
+package MediumProblems;
 
-   /*Given an integer x, return true if x is a palindrome,
-      and false otherwise. */
+import java.util.Collections;
+import java.util.Stack;
 
 public class Solution2 {
-        public boolean isPalindrome(int x) {
-            int r, sum = 0, temp;
-            temp = x;
-
-            while(x > 0){
-                r = x % 10;
-                sum = (sum * 10) + r;
-                x = x / 10;
-            }
-            if(temp==sum){
-                return true;
-            }
-            else{
-                return false;
+    public String reverseWords(String s) {
+        String[] str = s.split(" ");
+        String str2 = "";
+//        for (int i = str.length-1; i >= 0; i--){
+//            str2 = str2 + str[i];
+//            if (i!=0){
+//                str2 = str2 + " ";
+//            }
+//        }
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < str.length; i++){
+            if (!str[i].equals("")){
+                stack.push(str[i]);
             }
         }
+        int x = stack.size();
+        while (!stack.empty()){
+            str2 = str2 + stack.pop();
+            if (x!=1){
+                str2 = str2 + " ";
+            }
+            x--;
+        }
+
+        return str2;
     }
+}
+
+class Check{
+    public static void main(String[] args) {
+        Solution2 s2 = new Solution2();
+        System.out.println(s2.reverseWords(" a good  example"));
+    }
+}
